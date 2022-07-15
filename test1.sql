455a2139e4044335b182a5a83cab72dd2ffd2230
LinnikV <linnikvitek@ukr.net>
2022-07-15 22:00:30 +0300

    fixed first task v2.0

--- test1.sql
+++ test1.sql
@@ -3,10 +3,4 @@ select P.product_name, P.product_img_url, P.product_url, P.product_price_min, P.
 from grommet_products as P
 join grommet_product_categories as PC  on P.id = PC.product_id
 join grommet_gifts_categories as GP on PC.product_category_id = GP.id
-where GP.sub_category = 'Grilling' and not P.is_sold_out
-union all
-select P.product_name, P.product_img_url, P.product_url, P.product_price_min, P.product_short_description
-from grommet_products as P
-join grommet_product_categories as PC  on P.id = PC.product_id
-join grommet_gifts_categories as GP on PC.product_category_id = GP.id
-where GP.category = 'Outdoor Living' and not P.is_sold_out;
+where GP.sub_category = 'Grilling' and GP.category = 'Outdoor Living' and not P.is_sold_out;
